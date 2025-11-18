0e5a230c4f9aabacbd6a10a0542594e151b1dcef
Jon Bevan <jonathan.l.bevan@gmail.com>
2025-11-18 16:07:51 +0000

    added test folder and updated CI yml file

---
+++ tests/test_status_code.py
@@ -0,0 +1,8 @@
+import pytest
+from app import app
+
+def test_movies_status_code():
+
+    test_client = app.test_client()
+    response = test_client.get("movies")
+    assert response.status_code == 200
