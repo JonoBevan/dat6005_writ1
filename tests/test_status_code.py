e7c3e80e01b1b3a1b7f35ef651287e1b9f4c29b5
Jon Bevan <jonathan.l.bevan@gmail.com>
2025-11-18 17:05:47 +0000

    moved tests folder to same level as swapi_webapp, included a pytest.ini file to correct issue where test_status_code.py was not seeing the app.py file

---
+++ tests/test_status_code.py
@@ -0,0 +1,8 @@
+import pytest
+from swapi_webapp.app import app
+
+def test_movies_status_code():
+
+    test_client = app.test_client()
+    response = test_client.get("/movies")
+    assert response.status_code == 200
