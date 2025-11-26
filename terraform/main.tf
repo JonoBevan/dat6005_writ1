87eb7c10b4f77feca368b284868274eee2f1c5ff
Jon Bevan <jonathan.l.bevan@gmail.com>
2025-11-26 14:02:57 +0000

    added new SG for MySQL connection to EC2 instance in TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,12 +72,24 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # MySQL
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name = "aurora-rds-sg"
+  vpc_id = data.aws_vpc.default.id
+
   ingress {
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.web_sg.id]
+    description = "Allow MySQL/Aurora access from EC2"
   }
 
   egress {
@@ -87,7 +99,6 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 
 resource "aws_instance" "dat6005" {
   ami = data.aws_ami.amazon_linux.id
